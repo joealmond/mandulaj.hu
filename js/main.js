ff5220a5d47017bc912f19ef440e3bb740b3c1be
József Mandula <63676153+joealmond@users.noreply.github.com>
2021-12-04 10:25:18 +0100

    bug fixes

--- js/main.js
+++ js/main.js
@@ -261,17 +261,17 @@
   })();
 })(jQuery);
 
-var playPromise = document.querySelector(".intro-video").play();
+// var playPromise = document.querySelector(".intro-video").play();
 
 // In browsers that don’t yet support this functionality,
 // playPromise won’t be defined.
-if (playPromise !== undefined) {
-  playPromise
-    .then(function () {
-      // Automatic playback started!
-    })
-    .catch(function (error) {
-      // Automatic playback failed.
-      // Show a UI element to let the user manually start playback.
-    });
-}
+// if (playPromise !== undefined) {
+//   playPromise
+//     .then(function () {
+//       // Automatic playback started!
+//     })
+//     .catch(function (error) {
+//       // Automatic playback failed.
+//       // Show a UI element to let the user manually start playback.
+//     });
+// }
